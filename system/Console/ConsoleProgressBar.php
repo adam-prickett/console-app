dd17a47ac029d8712fe9cd1079082df9f4ce7e9b
Adam Prickett <adam.prickett@journolink.com>
2017-05-04 11:18:33 +0100

    * Add time elapsed and remaining estimate under progress bar * Add current memory usage after progress bar * Some formatting and comments

--- system/Console/ConsoleProgressBar.php
+++ system/Console/ConsoleProgressBar.php
@@ -19,13 +19,15 @@ trait ConsoleProgressBar
     protected $progressBarCurrentPercent = 0;
     protected $progressBarTitle;
     protected $progressBarCompleteMessage;
+    protected $progressBarStartTime;
 
     /**
      * Output and begin a progress bar
-     * @param  int $limit
+     * @param  int  $limit
+     * @param  int  $start
      * @return void
      */
-    public function progressBar($limit = 100, $start = 0)
+    public function progressBar(int $limit = 100, int $start = 0)
     {
         // Check that a progress bar isn't already running (we can only cope with 1)
         if ($this->progressBarStarted) {
@@ -37,9 +39,10 @@ trait ConsoleProgressBar
         $this->progressBarActive = false;
         $this->progressBarLimit = $limit;
         $this->progressBarCurrent = $start;
-        $this->progressBarCurrentPercent = floor($start/$limit);
+        $this->progressBarCurrentPercent = floor($start / $limit);
         $this->progressBarTitle = null;
         $this->progressBarCompleteMessage = null;
+        $this->progressBarStartTime = null;
 
         return $this;
     }
@@ -50,15 +53,16 @@ trait ConsoleProgressBar
      */
     public function startProgressBar()
     {
+        $this->progressBarStartTime = microtime(true);
         $this->drawProgressBar();
     }
 
     /**
-     * Set the Progress Bar title
+     * Set the title to display above the progress bar
      * @param  string $title
      * @return self
      */
-    public function setProgressBarTitle($title)
+    public function setProgressBarTitle(string $title)
     {
         $this->progressBarTitle = $title;
 
@@ -70,7 +74,7 @@ trait ConsoleProgressBar
      * @param  string $message
      * @return self
      */
-    public function setProgressBarCompleteMessage($message)
+    public function setProgressBarCompleteMessage(string $message)
     {
         $this->progressBarCompleteMessage = $message;
 
@@ -82,7 +86,7 @@ trait ConsoleProgressBar
      * @param  integer $num
      * @return void
      */
-    public function advanceProgressBar($num = 1)
+    public function advanceProgressBar(int $num = 1)
     {
         // Check that a progress bar has actually been started
         if (!$this->progressBarStarted) {
@@ -94,7 +98,7 @@ trait ConsoleProgressBar
             return;
         }
 
-        $this->progressBarCurrent = $this->progressBarCurrent+$num;
+        $this->progressBarCurrent = $this->progressBarCurrent + $num;
 
         $this->drawProgressBar();
     }
@@ -103,7 +107,7 @@ trait ConsoleProgressBar
      * Set the progress bar to $num
      * @param integer $num
      */
-    public function setProgressBar($num)
+    public function setProgressBar(int $num)
     {
         // Check that a progress bar has actually been started
         if (!$this->progressBarStarted) {
@@ -141,29 +145,77 @@ trait ConsoleProgressBar
      */
     private function drawProgressBar()
     {
-        $percentComplete = ceil(($this->progressBarCurrent/$this->progressBarLimit)*100);
-        $percentPerBlock = 100/$this->progressBarLength;
-        $numberOfBlocks = ceil($percentComplete/$percentPerBlock);
+        $percentComplete = ceil(($this->progressBarCurrent/$this->progressBarLimit) * 100);
+        $percentPerBlock = 100 / $this->progressBarLength;
+        $numberOfBlocks = ceil($percentComplete / $percentPerBlock);
 
         $this->progressBarCurrentPercent = $percentComplete;
 
-        // Print the progress bar
+        // Firstly, calculate the time that it's taken to get to this point to subtracting
+        // the current time from the start time. Then we need to work out how much time
+        // is taken per block and multiply by the total number of remaining blocks.
+        $timeElapsed = microtime(true) - $this->progressBarStartTime;
+        $timePerBlock = $timeElapsed / ($this->progressBarCurrent ?: 1);
+        $timeRemaining = $timePerBlock * ($this->progressBarLimit - $this->progressBarCurrent);
+
+        // Format and output the progress bar to the console. This will use VT100 escape
+        // codes to delete any existing lines and overwrite them. We start with  title
+        // if one is provided, followed by the progress bar and time elapsed/estimate
         if ($this->progressBarActive) {
-            // Move up 1 line and delete the entire line (via VT100 escape codes)
             if (!empty($this->progressBarTitle)) {
                 print("\033[1A\033[2K");
             }
             print("\033[1A\033[2K");
+            print("\033[1A\033[2K");
         }
+
         if (!empty($this->progressBarTitle)) {
             printf("\033[37;41m%s\033[0m", $this->progressBarTitle);
             print PHP_EOL;
         }
-        printf("%d/%d [\033[33m%s\033[0m] %d%%", $this->progressBarCurrent, $this->progressBarLimit, str_pad(str_repeat('#', $numberOfBlocks), 25, ' ', STR_PAD_RIGHT), $this->progressBarCurrentPercent);
+
+        printf(
+            "%d/%d [\033[33m%s\033[0m] %d%% (%.2fMb)",
+            $this->progressBarCurrent,
+            $this->progressBarLimit,
+            str_pad(str_repeat('#', $numberOfBlocks), 25, ' ', STR_PAD_RIGHT),
+            $this->progressBarCurrentPercent,
+            round(memory_get_usage() / 1024 / 1024, 2)
+        );
+        print PHP_EOL;
+        printf(
+            "%s elapsed / %s remaining",
+            $this->secondsToClock($timeElapsed),
+            $this->secondsToClock($timeRemaining)
+        );
         print PHP_EOL;
 
         $this->progressBarActive = true;
 
         return;
+    }
+
+    /**
+     * Converts a number of seconds into a human readable string and clock format
+     * @param  float  $ts
+     * @return string
+     */
+    private function secondsToClock(float $ts) : string
+    {
+        $days = floor($ts / 86400);
+        $hours = floor(($ts - ($days * 86400)) / 3600);
+        $minutes = floor(($ts - ($days * 86400) - ($hours * 3600)) / 60);
+        $seconds = floor($ts - ($days * 86400) - ($hours * 3600) - ($minutes * 60));
+
+        $string = '';
+        if (!empty($days)) {
+            $string .= sprintf('%d days, ', $days);
+        }
+
+        if (!empty($hours)) {
+            $string .= sprintf('%d hours, ', $hours);
+        }
+
+        return $string . sprintf('%02d:%02d', $minutes, $seconds);
     }
 }
